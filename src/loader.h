d5aa552ea24d5498406919bbeca0e2964143d090
Robert Pengelly <robertapengelly@Roberts-Air.lan>
2021-06-10 06:06:39 +0100

    Initial commit

---
+++ src/loader.h
@@ -0,0 +1,20 @@
+/* loader.h
+ * 
+ * Copyright 2002-2004 John Coffman
+ * Copyright 2009-2011 Joachim Wiedorn
+ * All rights reserved.
+ * 
+ * Licensed under the terms contained in the file 'COPYING'
+ * in the source directory.
+ */
+
+#include "common.h"
+
+extern BUILTIN_FILE First;
+extern BUILTIN_FILE Second;
+extern BUILTIN_FILE Third;
+extern BUILTIN_FILE Bitmap;
+extern BUILTIN_FILE Mbr;
+extern BUILTIN_FILE Mbr2;
+extern BUILTIN_FILE Chain;
+
---
+++ src/version.h
@@ -0,0 +1,19 @@
+/* version.h
+ * 
+ * Copyright 2005-2007 John Coffman
+ * Copyright 2009-2015 Joachim Wiedorn
+ * All rights reserved.
+ * 
+ * Licensed under the terms contained in the file 'COPYING'
+ * in the source directory.
+ */
+
+#ifndef VERSION_H
+#define VERSION_H
+
+#define VERSION_MAJOR 24
+#define VERSION_MINOR 2
+#define VERSION_EDIT  ""
+#define VERSION_DATE "22-November-2015"
+
+#endif
